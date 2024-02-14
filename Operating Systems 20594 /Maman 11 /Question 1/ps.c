2f38746c30d0456ba6a978b8b1c0b47e8d9073ee
OrelBriansev <104595505+OrelBriansev@users.noreply.github.com>
2024-02-14 18:11:11 +0200

    Add files via upload

---
+++ Operating Systems 20594 /Maman 11 /Question 1/ps.c
@@ -0,0 +1,12 @@
+//name:orel briantsev,ID:318264132,description:this file calling function cps and exiting
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(int argc,char *argv[])
+{
+    cps132();
+    exit();
+}
